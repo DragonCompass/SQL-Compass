ea246ff5e42099d5d909c48b61ef9f514e8463ec
synod2 <synod2@naver.com>
2020-11-17 01:55:26 +0900

    sqli finder

--- python/qchecker.py
+++ python/qchecker.py
@@ -14,7 +14,7 @@ def makeResult():
     data['vlistlen'] = len(vlist)
     data['vlist'] = vlist
     res = json.dumps(data,ensure_ascii=False)
-    # res = res.replace("\n",'')
+    res = res.replace("\n",'')
     return res
 
 def getJSON(href,fname,query,war):
@@ -25,9 +25,29 @@ def getJSON(href,fname,query,war):
     data["war"] = war
     return json.dumps(data,ensure_ascii=False)
 
+def checkSQLi(href): #find SQL injection
+    global slistlen
+    #for q in aQlist : 
+    q = " or 1=1"
+    retlist = checkNormal(href,q)
+    reslist = []
+
+    for s in href.arglist : 
+        for i in retlist : 
+            for j in retlist : 
+                if i!=j : 
+                    if checkResSame(i,j):
+                        reslist.append(i)
+                        reslist.append(j)
+        reslist = list(set(reslist))
+        if len(reslist) >= 2 : 
+            vlist.append(getJSON(href,s.name,q,"high"))
+        else :  
+            slistlen += 1
+            # print("SQL injection Occured!")
+
 def checkVOper(href): #check operater is worked in query
     global slistlen
-    retlist = []
     for s in href.arglist :
         if(s.atype == "digit"):
             cq = str(( int(s.oval)+1)) + '-1'
@@ -39,50 +59,53 @@ def checkVOper(href): #check operater is worked in query
                 slistlen += 1
 
 def checkNormal(href,q):
-    an = makeAnormal(href)
-    res = href.classmember(q)
-    # compare with anormal result.
-    if checkResSame(an,res) : #this result is anormal 
-        return False
-    else :  #this result is normal 
-        return True
+    an,nl = makeAnormal(href)
+    retlist = []
+    for s in nl : 
+        # print("query : "+s+q)
+        res = href.classmember(s+q)
+        # compare with anormal result.
+        if checkResSame(an,res) : #this result is anormal 
+            pass
+        else :  #this result is normal 
+            if checkError(res) : 
+                retlist.append(res)
+    return retlist
         
 def makeAnormal(href) : #find anormal result
     qlist = ['1','2','3','4','5','a','b','c','d','-1','a1','1a']
-    nq = [] #save requests return  
-    an = [] #save anormally returns
+    neq = [] #non-error querys 
+    an = [] #anormally returns
+    nl = [] #normal querys    
     
     for q in qlist :
         res = href.classmember(q)
         if checkError(res) :
-            nq.append(res)
-        # else :
-        #     elist.append(getJSON(href,))
-
-    rlen = len(nq)
+            neq.append(res)
+        
+    rlen = len(neq) #normal query
 
     for i in range(0,rlen) :
         for j in range(0,rlen) :
             if i != j : 
-                if checkResSame(nq[i],nq[j]) : 
-                    an.append(nq[i])
-                    an.append(nq[j])
+                if checkResSame(neq[i],neq[j]) : 
+                    an.append(neq[i])
+                    an.append(neq[j])
                 
     an = list(set(an))    
-    nq = list(set(nq))
-    qlist.remove(an)
-    qlist.remove(nq)
-    print(qlist)
+    for s in neq: 
+        nl.append(s[2])
+    for s in an:
+        nl.remove(s[2])   
 
-    print(an)
-    return an[0]
+    return an[0],nl
 
 def checkResSame(res1,res2): #check res1 and res2 request result is same
     # res1 = href.classmember(q1)
     # res2 = href.classmember(q2)    
     q1 = res1[2]
     q2 = res2[2]
-    
+
     if (res1[1] != res2[1])  : # two results return code is not same
         return False
     elif not (checkError(res1) and checkError(res1)) : 
@@ -93,7 +116,11 @@ def checkResSame(res1,res2): #check res1 and res2 request result is same
     else : 
         q3 = q2.replace(q1,'')
 
-    res = cr.show_diff(res1[0],res2[0])
+    res1_ = res1[0].replace(q1,'')
+    res2_ = res2[0].replace(q2,'')
+
+    res = cr.show_diff(res1_,res2_)
+    
     for s in res[1] : #s is differ parts
         if ((q1 != s ) and (q2 != s )  and (q3 != s )) : # two results are not same 
             return False

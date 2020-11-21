e6b7b51599e9ce26214795920b1c396d2a42bcc4
yujiyea <54650556+yujiyea@users.noreply.github.com>
2020-11-22 03:58:12 +0900

    jquery

--- web/handle.php
+++ web/handle.php
@@ -1,19 +1,128 @@
 <?php
-    #exec("python python/tool.py",$str);
-    #exec("cd /var/www/html/KSJ_SQLI_Project/python/dist && ./tool", );
-    exec("cd /var/www/html/python/dist && ./tool",$str);
+    #exec("cd /var/www/html/KSJ_SQLI_Project/python/dist && ./tool", $str);
+    if(isset($_POST["mode"])){
+        $mode = $_POST["mode"];
+        exec("tool.exe".$mode,$str);
+        if($mode==" -m result"){
+            echo implode($str);
+        }else{
+            echo $str[0];
+        }
+        
+        
+        #if($mode==" -m result"){
+        #$result = json_decode($str[0],true);
+        /*$check_page_num=$result['apagelen'];
+        $vul_page_num=$result['vpagelen'];
+        $page_ratio=round($vul_page_num/$check_page_num*100,1);
+        $vul_num=$result['vparlen'];
+        $high_vul_num=$result['high'];
+        $low_vul_num=$result['low'];
+        $form_num =$result['aparlen'];
+
+        #동그라미 도표 데이터
+        #$normal = round($check_page_num-$vul_page_num/$check_page_num*100,0);
+        $normal = $result['sparlen'];
+        $high = round($high_vul_num/$form_num*100,0);
+        $low = round($low_vul_num/$form_num*100,0);
+
+        $barlist = $result['vpage'];
+        $barData=Array();
+        foreach ($result['vpage'] as $tmp1){
+            $barlist = json_decode($tmp1,true);
+            array_push($barData,
+                array(
+                    $barlist['url'],
+                    $barlist['fname'],
+                    $barlist['query'],
+                )
+            );
+        }
+
+        $vlist= $result['vlist'];
+        $tableData=Array();
+        foreach ($result['vlist'] as $tmp2){
+            $vlist = json_decode($tmp2,true);
+            array_push($tableData,
+                array(
+                    $vlist['url'],
+                    $vlist['fname'],
+                    $vlist['query'],
+                )
+            );
+        }}*/
+    }
+    else{
+        exec("tool.exe",$str);
+        echo $str[0];
+    }
+    
+    /*if(isset($_POST["url"]))
+        $url = $_POST["url"];
+    if(isset($_POST["option"]))
+        $option = $_POST["option"];
+
+    $repeatCheck ="";
+
+    if(isset($_POST["href"]))
+        $href = $_POST["href"];
+    if(isset($_POST["flist"]))
+        $flist= $_POST["flist"];
+    if(isset($_POST["formNum"]))
+        $formNum = $_POST["formNum"];
+    if(isset($_POST["repeatCheck"]))
+        $repeatCheck = $_POST["repeatCheck"];
+
+    if($url){
+        #exec("cd C:\xampp\htdocs\project(kshild)\python && tool.exe",$str);
+        exec("tool.exe",$str);
+        echo $str[0];
+    }elseif ($href!=-1 && $flist==-1) {
+        if($repeatCheck=="h1" && $href !=$formNum){
+            exec("tool.exe -pl hreflist -pt sqli -pi $href",$str);
+            echo $str[0];
+        }else if($repeatCheck=="h1" && $href==formNum){
+            $repeatCheck="h2";
+            echo $str[0]."^".$repeatCheck;
+        }
+        exec("tool.exe -pl hreflist -pt voper -pi $href",$str);
+//        $data = $str[0]."^".$repeatCheck;
+        if($repeatCheck==""&&$href==$formNum){
+            $repeatCheck="h1";
+            echo $str[0]."^".$repeatCheck;    
+        }else{
+            echo $str[0];
+        }
+    }elseif ($flist!=-1){
+        if($repeatCheck=="f1"){
+            exec("tool.exe -pl flist -pt sqli -pi $href",$str);
+            echo $str[0]."^".$repeatCheck;
+        }
+        exec("tool.exe -pl flist -pt voper -pi $href",$str);
+        if($repeatCheck==""&&$flist==$formNum-1){
+            $repeatCheck="f1";
+            echo $str[0]."^".$repeatCheck;    
+        }else{
+            echo $str[0];
+        }
+    }else if($href==$formNum && $repeatCheck=="h2"){
+        exec("tool.exe -m result",$str);
+        echo $str[0];
+    }*/
+/*    
     $result = json_decode($str[0],true);
     $check_page_num=$result['alistlen'];
     $vul_page_num=$result['vpagelen'];
-    $page_ratio=$vul_page_num/$check_page_num*100;
+    $page_ratio=round($vul_page_num/$check_page_num*100,1);
     $vul_num=$result['vlistlen'];
     $high_vul_num=$result['high'];
     $low_vul_num=$result['low'];
 
     #동그라미 도표 데이터
-    $normal = $check_page_num-$vul_page_num/$check_page_num*100;
-    $high = $high_vul_num/$check_page_num*100;
-    $low = $low_vul_num/$check_page_num*100;
+    #$normal = round($check_page_num-$vul_page_num/$check_page_num*100,0);
+    $normal = round($vul_num/$check_page_num*100,0);
+    $high = round($high_vul_num/$check_page_num*100,0);
+    $low = round($low_vul_num/$check_page_num*100,0);
 
 
     $vlist= $result['vlist'];
@@ -23,18 +132,19 @@
 
     #$tableData=[{'url':'a','form':'b','query':'c'},{'url':'a','form':'b','query':'c'},{'url':'a','form':'b','query':'c'}
     #,{'url':'a','form':'b','query':'c'},{'url':'a','form':'b','query':'c'},{'url':'a','form':'b','query':'c'}];
-    $tableData = [['a','b','c'],['a','b','c'],['a','b','c']];
-    $tableData=Array();
-    foreach ($result['vlist'] as $tmp){
-            $vlist = json_decode($tmp,true);
-            array_push($tableData,
-                    array(
-                            $vlist['url'],
-                            $vlist['fname'],
-                            $vlist['query'],
-                    )
-            );
-    } 
+$tableData=Array();
+foreach ($result['vlist'] as $tmp){
+	$vlist = json_decode($tmp,true);
+	array_push($tableData,
+		array(
+			$vlist['url'],
+			$vlist['fname'],
+			$vlist['query'],
+		)
+	);
+}
+
+#   $tableData = [['a','b','c'],['a','b','c'],['a','b','c']];
 
 
     $datav=10203;
@@ -46,12 +156,13 @@
     echo ("<script> var data2= new Array($datav,'2000','3000','4000','5000','6000') </script>");
     #echo ("<script> var data2= $data2 </script>");
 
-    #동그라미 도표 변경
+
+    
+#동그라미 도표 변경
     #echo ("<script type='text/javascript' src='js/demo/chart-pie-demo.js'>myPieChart.data.datasets[0].data=[$normal,$high,$low]</script>");
     #echo ("<script type='text/javascript' src='js/demo/chart-bar-demo.js'>updateBarGraph(data1,data2)</script>");
 
     #echo ("<script> var pieData= new Array($normal,$high,$low) </script>");
-    echo ("<script> var pieData= new Array(70,20,10) </script>");
-
-
+    echo ("<script> var pieData= new Array(71,20,10) </script>");
+    */
 ?>
--- web/index.php
+++ web/index.php
@@ -20,16 +20,49 @@
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet" />
+<style>
+	#loading{
+	background-color : white;
+	height : 50vh;
+	width : 50vw;
+	z-index : 3;
+	position : absolute;
+	top : 25vh;
+	left : 25vw;
+	display : none;		
+	}
+	#mask {
+		background-color : black;
+		opacity : 0.5;
+		display : none;
+		z-index : 2;
+		position : absolute;
+		height : 100vh;
+		width : 100vw;	
+		top : 0;
+		left : 0;
+}	
+</style>
 
+    <script>
+        function check_input(){
+            if(!document.inner_form.search.value){
+                alert("검사할 페이지 주소를 입력해주세요!");
+                document.inner_form.search.focus();
+                return;
+            }
+            document.inner_form.submit();
+        }
+    </script>
 </head>
     <body>
         <div class="s003">
-        <form>
+        <form id="inject" >
             <div class="inner-form">
                 <div class="input-field first-wrap">
                     <div class="input-select">
-                    <select data-trigger="" name="choices-single-defaul">
-                        <option placeholder="">현재 페이지 검사</option>
+                    <select data-trigger="" id='option' name="choices-single-defaul">
+                        <option placeholder="" selected>현재 페이지 검사</option>
                         <option>하위 페이지까지 검사</option>
                     </select>
                     </div>
@@ -38,7 +71,7 @@
                     <input id="search" type="text" placeholder="검사할 페이지 주소 입력..." />
                 </div>
                 <div class="input-field third-wrap">
-                    <button class="btn-search" type="button" onclick="location.href='result.php'">
+                     <button class="btn-search" type="button" > <!--onclick="check_input()"-->
                     <svg class="svg-inline--fa fa-search fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
                     </svg>
@@ -47,8 +80,35 @@
             </div>
         </form>
         </div>
+	<div id="loading">
+		<a>SQL injection 취약점 검사중입니다.</a>
+	</div>
+	<div id="mask">
+	</div>
         <script src="js/extention/choices.js"></script>
+	<script src="vendor/jquery/jquery.min.js"></script>
+
         <script>
+	$('.btn-search').click(function(){
+                var newform = $('<form></form>');
+		newform.attr('action','result.php');
+		newform.attr('method','post');
+
+		$('#loading').css('display','block');
+        $('#mask').css('display','block');
+		uval = $('#search').val()
+		opt = $('#option').val()
+		if (opt == "현재 페이지 검사")
+			opt = 0; // one page
+		else 
+			opt = 1; // all page 
+		newform.append($('<input/>',{type:'text',name:'url',value:uval}));
+		newform.append($('<input/>',{type:'text',name:'option',value:opt}));
+
+		newform.appendTo('body');
+		newform.submit();
+	});
+
         const choices = new Choices('[data-trigger]',
         {
             searchEnabled: false,

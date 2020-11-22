22f5c97274376fb9c70e03a18cc9ac15f0d1526b
synod2 <synod2@naver.com>
2020-11-22 22:56:25 +0900

    change

--- python/qchecker.py
+++ python/qchecker.py
@@ -3,85 +3,143 @@ from collections import OrderedDict
 import json
 import aQlist
 
-vlist = []
-slist = []
-slistlen = 0
-elist = []
+vlist = [] 
+vdict = [] # vlist 안의 값 타입이 dict가 아니라 참조가불가능해서 따로만듬
 
-def makeResult():
+slist = [] # secure, 안전한 사이트 리스트( append is (if warlist in clist))
+slistlen = 0
+
+def makeResult(page):
     data = OrderedDict()
-    data['alistlen'] = slistlen+len(vlist)
-    data['vlistlen'] = len(vlist)
+    data['spagelen'] = 0
+    data['vpagelen'] = 0
+    data['aparlen'] = 0
+    data['vparlen'] = 0
+    data['low'] = 0
+    data['high'] = 0
+    vpage = []
+
+    for h in page.hreflist : 
+        tmp = OrderedDict()
+        tmp['url'] = h.url
+        tmp['safe'] = []
+        tmp['high'] = []
+        tmp['low'] = []
+        if (h.vul) != "safe" : 
+            data['vpagelen'] += 1 
+            # print(h.url)
+            for a in h.arglist: 
+                data['aparlen'] += 1
+                if a.vul != "safe" : 
+                    data['vparlen'] += 1
+                    if a.vul == "high" : 
+                        tmp['high'].append(a.name)
+                        data['high'] += 1
+                    else :
+                        tmp['low'].append(a.name)
+                        data['low'] += 1
+                else : 
+                    tmp['safe'].append(a.name)
+        else : 
+            for a in h.arglist: 
+                data['aparlen'] += 1
+                tmp['safe'].append(a.name)
+            data['spagelen'] += 1  
+            
+        vpage.append(json.dumps(tmp,ensure_ascii=False,indent="\t"))
+
+    data['apagelen'] = data['spagelen'] + data['vpagelen'] 
+    data['sparlen'] = data['aparlen'] - data['vparlen'] 
     data['vlist'] = vlist
+    data['vpage'] = vpage
+    
     res = json.dumps(data,ensure_ascii=False,indent="\t")
-    res = res.replace("\n",'')
+    # print(data)
+    # print(vlist)
     return res
 
-def getJSON(href,fname,query,war):
+def getJSON(href,fname,query,war,method):
     data = OrderedDict()
     data["url"] = href.baseurl + href.url
     data["fname"] = fname
     data["query"] = query
     data["war"] = war
+    data['method'] = method
     return json.dumps(data,ensure_ascii=False,indent="\t")
-
-def checkSQLi2(href): #find SQL injection
+ 
+# aQlist(SQL Cheat list) 주입 하여 high(고위험)페이지 판별
+def checkSQLi(href): #find SQL injection
     global slistlen
+    
     for q in aQlist.qlist : 
         retlist = checkNormal(href,q)
         reslist = []
-
-        for s in href.arglist : 
+        # print(retlist)
+        if "hrefset" in str(type(href)) :
+            for s in href.arglist : 
+                for i in retlist : 
+                    for j in retlist : 
+                        if i!=j : 
+                            if checkResSame(i,j):
+                                reslist.append(i)
+                                reslist.append(j)
+                reslist = list(set(reslist))
+        elif "formset" in str(type(href)) :
             for i in retlist : 
+                print(i)    
                 for j in retlist : 
                     if i!=j : 
                         if checkResSame(i,j):
                             reslist.append(i)
                             reslist.append(j)
             reslist = list(set(reslist))
-            if len(reslist) >= 2 : 
-                vlist.append(getJSON(href,s.name,q,"high"))
-            else :  
-                slistlen += 1
-
-def checkSQLi(href): #find SQL injection
-    global slistlen
-    #for q in aQlist : 
-    q = " or 1=1"
-    retlist = checkNormal(href,q)
-    reslist = []
-
-    for s in href.arglist : 
-        for i in retlist : 
-            for j in retlist : 
-                if i!=j : 
-                    if checkResSame(i,j):
-                        reslist.append(i)
-                        reslist.append(j)
-        reslist = list(set(reslist))
+        
         if len(reslist) >= 2 : 
-            vlist.append(getJSON(href,s.name,q,"high"))
+            s.vul = "high"
+            href.vul = "high" 
+            vlist.append(getJSON(href,s.name,q,"high",href.method))
+            #vlist.append(getJSON(href,s.name,q,"high"))
+            #vdict.append({"url" : href.baseurl + href.url, "fname":s.name, "query":q, "war":"high"})
         else :  
             slistlen += 1
+    # print(reslist)
+    # print(str(type(href)))
+    if "hrefset" in str(type(href))  :
+        print(len(href.arglist))
+    elif "formset" in str(type(href))  :
+        print(len(href.namelist))
+
+# 쿼리내에서 operater(연산자) 동작을 확인하여 low(저위험)페이지 판별
+# href = hreflist, get method의 argument 에 1(base val)과 2(base val +1) -1 을 진행.
 
 def checkVOper(href): #check operater is worked in query
     global slistlen
+    count = 0
     for s in href.arglist :
+        count += 1
         if(s.atype == "digit"):
             cq = str(( int(s.oval)+1)) + '-1'
-            res1 = href.classmember(s.oval)
-            res2 = href.classmember(cq)    
-            if checkResSame(res1,res2) : 
-                vlist.append(getJSON(href,s.name,cq,"low"))
+            res1 = href.dosqli(s.oval)
+            res2 = href.dosqli(cq)    
+            # res1, res2의 결과값이 같다면 operator 동작으로 low 취약점 리스트에 세팅.
+            if checkResSame(res1,res2) :
+                s.vul = "low"
+                href.vul = "low" 
+                vlist.append(getJSON(href,s.name,cq,"low",href.method))
+                #vdict.append({"url" : href.baseurl + href.url, "fname":s.name, "query":cq,"war":"low"})
             else : 
+                # slist에 안전한 사이트들을 저장
+                slist.append(str(href.baseurl + href.url))
                 slistlen += 1
+    # href.getdata()
+    print(count)
 
 def checkNormal(href,q):
     an,nl = makeAnormal(href)
+    print(nl)
     retlist = []
     for s in nl : 
-        # print("query : "+s+q)
-        res = href.classmember(s+q)
+        res = href.dosqli(s+q)
         # compare with anormal result.
         if checkResSame(an,res) : #this result is anormal 
             pass
@@ -91,13 +149,14 @@ def checkNormal(href,q):
     return retlist
         
 def makeAnormal(href) : #find anormal result
-    qlist = ['1','2','3','4','5','a','b','c','d','-1','a1','1a']
+    qlist = ['1','2','3','a','b','c','0','-1','-2']
     neq = [] #non-error querys 
     an = [] #anormally returns
     nl = [] #normal querys    
     
     for q in qlist :
-        res = href.classmember(q)
+        res = href.dosqli(q)
+        # print(res)
         if checkError(res) :
             neq.append(res)
         
@@ -115,18 +174,23 @@ def makeAnormal(href) : #find anormal result
         nl.append(s[2])
     for s in an:
         nl.remove(s[2])   
-
+    if (len(an) == 0) :
+        an.append(" ")
+    if (len(nl) == 0) :
+        nl.append(" ")
     return an[0],nl
 
+
+# 두 페이지를 비교하여 결과값이 같다면 True 를 리턴한다.
 def checkResSame(res1,res2): #check res1 and res2 request result is same
-    # res1 = href.classmember(q1)
-    # res2 = href.classmember(q2)    
+    # res1 = href.dosqli(q1)
+    # res2 = href.dosqli(q2)    
     q1 = res1[2]
     q2 = res2[2]
 
     if (res1[1] != res2[1])  : # two results return code is not same
         return False
-    elif not (checkError(res1) and checkError(res1)) : 
+    elif not (checkError(res1) and checkError(res2)) : 
         return False
 
     if len(q1) > len(q2): #check if string has includes the other one.
